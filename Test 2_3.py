d0192e14db91096bdb1edc66900246b52ae7069d
arssanihanna <132549220+arssanihanna@users.noreply.github.com>
2023-05-04 11:39:20 +0200

    Add files via upload

---
+++ Test 2_3.py
@@ -0,0 +1,30 @@
+int_a = int(input("Zahl 1:"))
+
+# if [Bedingung] :
+#if int_a == 0:
+    #print("AUFPASSEN! Zahl 1  = 0 !!!")
+    
+    #int_a = int(input("Andere Zahl: "))
+
+if int_a > 0:
+    print("Super gemacht, Sie haben eine größerer zahl als 0 eingegeben!")
+    
+#if int_a < 0:
+    #print("BRO -  gib eine Zahl ein, die größer als 0 ist!")
+    #int_a = int(input("Andere Zahl: "))
+    
+# while [Bedingung] :
+# Anweisungen...
+while int_a < 0:
+    print("BRO -  gib eine Zahl ein, die größer als 0 ist!")
+    int_a = int(input("Andere Zahl: "))
+    
+while int_a == 0:
+    print("AUFPASSEN! Zahl 1  = 0 !!!")
+    
+    int_a = int(input("Andere Zahl: "))
+
+#while int_a <= 10:
+   # print(int_a)
+  #  int_a = int_a + 1
+    
