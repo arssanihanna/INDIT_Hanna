eb2d359dffa09a89d8b5bdd8e95d3201242e972b
arssanihanna <132549220+arssanihanna@users.noreply.github.com>
2023-05-04 10:31:41 +0200

    Add files via upload

---
+++ Test 1_2.py
@@ -0,0 +1,13 @@
+x = input("Geben Sie hier Ihre 1.Eingabe ein:")
+z= ("Hello"+x)
+
+str_a = input("Zahl 1:")
+int_a = int(str_a)
+
+str_b = input("Zahl 2:")
+int_b = int(str_b)
+
+Plus = int_a + int_b
+Minus = int_a - int_b
+Maltiplikation = int_a * int_b
+Division = int_a / int_b
